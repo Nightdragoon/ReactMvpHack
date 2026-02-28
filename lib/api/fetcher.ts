fc28b9239e65dde655b8ec6d1d907bfc965b671a
Nightdragoon <crackytrini@gmail.com>
2026-02-27 22:56:06 -0600

    implementacion de endponts
    implementacion

--- lib/api/fetcher.ts
+++ lib/api/fetcher.ts
@@ -17,3 +17,46 @@ export const fetcTest = async () => {
         throw error;
     }
 };
+
+// endpoints para productos
+export const fetchGetAllProductos = async (solo_activos: boolean = false) => {
+    try {
+        const response = await axios.get(
+            API_ENDPOINTS.getAllProductos,
+            { params: { solo_activos } }
+        );
+        console.log("Respuesta de fetchGetProuctos:", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Error al obtener productos:", error);
+        throw error;
+    }
+};
+
+export const fetchPostProducto = async (precio: number, nombre: string, activo: number) => {
+    try {
+        const response = await axios.post(
+            API_ENDPOINTS.guardarProducto,
+            {
+                precio,
+                nombre,
+                activo
+            },
+            {
+                headers: {
+                    'accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        console.log("Respuesta de fetchPostProducto:", response.data);
+        return response.data;
+
+    } catch (error) {
+        console.error("Error al crear producto:", error);
+        throw error;
+    }
+};
+
+
+
